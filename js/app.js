502382453c823837bc54c50df6446ee3992badf8
toran billups <toranb@gmail.com>
2013-10-01 18:19:38 -0500

    added rsvp to the mix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-function stubEndpointForHttpRequest(url, json) {
+function stubEndpointForHttpRequest(url, json, status) {
+    if (status == null) {
+        status = 200;
+    }
     $.mockjax({
+        type: "GET",
         url: url,
+        status: status,
         dataType: 'json',
         responseText: json
     });
@@ -11,8 +16,9 @@ $.mockjaxSettings.responseTime = 0;
 
 var people = [{firstName: 'toran', lastName: 'billups'}, {firstName: 'todd', lastName: 'zombie'}];
 var cats = [{firstName: 'cat', lastName: 'hat'}, {firstName: 'mat', lastName: 'rat'}];
-stubEndpointForHttpRequest('/api/people/', people);
-stubEndpointForHttpRequest('/api/cats/', cats);
+stubEndpointForHttpRequest('/api/people/', people, 200);
+stubEndpointForHttpRequest('/api/cats/', cats, 200);
+//stubEndpointForHttpRequest('/api/cats/', "ajax blew up", 400);
 
 App = Ember.Application.create();
 
@@ -62,18 +68,26 @@ App.Person.reopenClass({
     },
     find: function() {
         var self = this;
-        $.getJSON('/api/people/').then(function(response) {
-            response.forEach(function(hash) {
-                var person = App.Person.create(hash);
-                Ember.run(self.people, self.people.pushObject, person);
-            })
+
+        var peoplePromise = new Ember.RSVP.Promise(function(resolve, reject) {
+            $.getJSON('/api/people/', resolve).fail(reject);
         });
-        $.getJSON('/api/cats/').then(function(response) {
-            response.forEach(function(hash) {
-                var cat = App.Person.create(hash);
-                Ember.run(self.people, self.people.pushObject, cat);
-            })
+
+        var catPromise = new Ember.RSVP.Promise(function(resolve, reject) {
+            $.getJSON('/api/cats/', resolve).fail(reject);
         });
+
+        Ember.RSVP.all([peoplePromise, catPromise]).then(function(things) {
+            things.forEach(function(thing) {
+                thing.forEach(function(hash) {
+                    var thing = App.Person.create(hash);
+                    Ember.run(self.people, self.people.pushObject, thing);
+                });
+            });
+        }, function(value) {
+            alert(value.status + ": promise failed " + value.responseText);
+        });
+
         return this.people;
     }
 });
